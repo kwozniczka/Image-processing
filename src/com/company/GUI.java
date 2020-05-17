6f14167bf7e87425f6c5b823a03454c76496f17b
Karolina <kj.wozniczka@gmail.com>
2020-05-17 14:41:10 +0200

    Initial commit

---
+++ src/com/company/GUI.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame {
+
+    JButton buttonQuitProgram;
+    JButton buttonOpenFile;
+
+    public GUI() {
+        //setSize(1280,720);
+        setSize(640, 900);
+        setTitle("Aplikacja Obrazy");
+        setLayout(null);
+
+//        buttonOpenFile = new JButton("Otwórz obraz");
+//        buttonOpenFile.setBounds(50, 50, 200, 50); // nadanie parametrow przyciskowi
+//        add(buttonOpenFile);
+//        buttonOpenFile.addActionListener(this);
+//
+//        buttonQuitProgram = new JButton("Wyjście");
+//        buttonQuitProgram.setBounds(450, 325, 100, 50);
+//        add(buttonQuitProgram);
+//        buttonQuitProgram.addActionListener(this);
+    }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // okno dialogowe
+        GUI gui = new GUI();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setVisible(true);
+    }
+}
